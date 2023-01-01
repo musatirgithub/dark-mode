80aa5443d12a32d0dd35abc59578f8b52e209d84
Mehmet Ugur SATIR <musatir@gmail.com>
2023-01-01 21:16:09 +0300

    first entrance

--- src/data.js
+++ src/data.js
@@ -1,7 +1,11 @@
 export const data = [
-    {
-        id:1,
-        title:
-        
-    }
-]
+  {
+    id: 1,
+    title:
+      "Biden signs TikTok ban for government devices, setting up a chaotic 2023 for the app",
+    date: new Date(2023, 01, 01),
+    length: 5,
+    description:
+      "The ban came as part of the 4,126-page spending bill and follows several state government bans of the app.",
+  },
+];

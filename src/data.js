00cdb63257d834a7332d0e6b078b3a5b8bc24514
Mehmet Ugur SATIR <musatir@gmail.com>
2023-01-01 21:57:01 +0300

    data file bug fix

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const data = [
     id: 1,
     title:
       "Biden signs TikTok ban for government devices, setting up a chaotic 2023 for the app",
-    date: new Date(2023, 01, 01),
+    date: new Date(2023, 1, 1),
     length: 5,
     description:
       "The ban came as part of the 4,126-page spending bill and follows several state government bans of the app.",
@@ -12,7 +12,7 @@ export const data = [
     id: 2,
     title:
       "You against the machine: Can you spot which image was created by AI?",
-    date: new Date(2023, 01, 02),
+    date: new Date(2023, 1, 2),
     length: 10,
     description:
       "AI art is suddenly everywhere. We created a quiz to see whether you can spot the difference between human- and robot-generated images.",
@@ -21,7 +21,7 @@ export const data = [
     id: 3,
     title:
       "Elon Musk tells Tesla employees don’t be 'bothered by stock market craziness'",
-    date: new Date(2023, 01, 03),
+    date: new Date(2023, 1, 3),
     length: 12,
     description:
       "Tesla shares have plummeted 42% in December and are poised to close out their worst month, quarter and year on record.",
@@ -29,7 +29,7 @@ export const data = [
   {
     id: 4,
     title: "TikTok banned on all House-issued mobile devices",
-    date: new Date(2023, 01, 04),
+    date: new Date(2023, 1, 4),
     length: 11,
     description:
       "The chief administrative officer of the House, who issued the directive, deemed the popular app a security risk.",

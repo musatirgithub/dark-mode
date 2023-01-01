1412d9dcc5ccf651ae1fdd3b03fda7b6145a9aa2
Mehmet Ugur SATIR <musatir@gmail.com>
2023-01-01 21:18:47 +0300

    second data is updated

--- src/data.js
+++ src/data.js
@@ -8,4 +8,31 @@ export const data = [
     description:
       "The ban came as part of the 4,126-page spending bill and follows several state government bans of the app.",
   },
+  {
+    id: 2,
+    title:
+      "You against the machine: Can you spot which image was created by AI?",
+    date: new Date(2023, 01, 02),
+    length: 10,
+    description:
+      "AI art is suddenly everywhere. We created a quiz to see whether you can spot the difference between human- and robot-generated images.",
+  },
+  {
+    id: 3,
+    title:
+      "You against the machine: Can you spot which image was created by AI?",
+    date: new Date(2023, 01, 02),
+    length: 10,
+    description:
+      "AI art is suddenly everywhere. We created a quiz to see whether you can spot the difference between human- and robot-generated images.",
+  },
+  {
+    id: 4,
+    title:
+      "You against the machine: Can you spot which image was created by AI?",
+    date: new Date(2023, 01, 02),
+    length: 10,
+    description:
+      "AI art is suddenly everywhere. We created a quiz to see whether you can spot the difference between human- and robot-generated images.",
+  },
 ];

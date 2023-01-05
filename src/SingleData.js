248b3498536c2a532add8441a75c2a4253b758f3
Mehmet Ugur SATIR <musatir@gmail.com>
2023-01-06 00:29:43 +0300

    data update

--- src/SingleData.js
+++ src/SingleData.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SingleData = ({ id, title, date, length, description }) => {
   return (
-    <article>
+    <article className="single-data">
       <h4>{title}</h4>
       <h5>{`Publication date: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`}</h5>
       <h5>{`Estimated Read Duration: ${length} mins`}</h5>
--- src/data.js
+++ src/data.js
@@ -1,37 +1,32 @@
 export const data = [
   {
     id: 1,
-    title:
-      "Biden signs TikTok ban for government devices, setting up a chaotic 2023 for the app",
+    title: "Biden signs TikTok ban",
     date: new Date(2023, 1, 1),
     length: 5,
-    description:
-      "The ban came as part of the 4,126-page spending bill and follows several state government bans of the app.",
+    description: "The ban came as part of the 4,126-page spending bill.",
   },
   {
     id: 2,
-    title:
-      "You against the machine: Can you spot which image was created by AI?",
+    title: "You against the machine",
     date: new Date(2023, 1, 2),
     length: 10,
     description:
-      "AI art is suddenly everywhere. We created a quiz to see whether you can spot the difference between human- and robot-generated images.",
+      "Can you spot the difference between human and robot generated images?",
   },
   {
     id: 3,
-    title:
-      "Elon Musk tells Tesla employees don’t be 'bothered by stock market craziness'",
+    title: "Tesla employees bothered by stock prices",
     date: new Date(2023, 1, 3),
     length: 12,
-    description:
-      "Tesla shares have plummeted 42% in December and are poised to close out their worst month, quarter and year on record.",
+    description: "Tesla shares have plummeted 42% in December.",
   },
   {
     id: 4,
-    title: "TikTok banned on all House-issued mobile devices",
+    title: "TikTok banned on House mobile devices",
     date: new Date(2023, 1, 4),
     length: 11,
     description:
-      "The chief administrative officer of the House, who issued the directive, deemed the popular app a security risk.",
+      "The chief administrative officer of the House sees TikTok as a security risk.",
   },
 ];

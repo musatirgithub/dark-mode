63f8c67650e0a37a6962001eae6b550ea7a971aa
Mehmet Ugur SATIR <musatir@gmail.com>
2023-01-06 00:49:23 +0300

    theme button added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import News from "./News";
 
 function App() {
--- src/News.js
+++ src/News.js
@@ -1,10 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import { data } from "./data";
 import SingleData from "./SingleData";
 
+const getTheme = () => {
+  let theme;
+  localStorage.getItem("theme")
+    ? (theme = localStorage.getItem("theme"))
+    : (theme = "light-theme");
+  return theme;
+};
+
 const News = () => {
+  const [theme, setTheme] = useState(getTheme());
+
   return (
     <main>
+      <button>{`${
+        theme === "light-theme" ? "dark-theme" : "light-theme"
+      }`}</button>
       {data.map((item) => {
         return <SingleData key={item.id} {...item} />;
       })}
